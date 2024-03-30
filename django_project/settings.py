d3a83e59e7c38728183cda973d198ee21cd35968
yoshihiro-naka <ynaka@rakuten.jp>
2024-03-31 00:05:39 +0900

    news paper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 	# Local
 	"accounts",
 	"pages",
+	"articles",
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
--- django_project/urls.py
+++ django_project/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include  # new
-from django.views.generic.base import TemplateView  # new
+from django.urls import path, include  
+# from django.views.generic.base import TemplateView  
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),  # new
-    path("accounts/", include("django.contrib.auth.urls")),  # new
-    path("", include("pages.urls")),  # new
+    path("accounts/", include("accounts.urls")),  
+    path("accounts/", include("django.contrib.auth.urls")),  
+    path("articles/", include("articles.urls")),  # new
+    path("", include("pages.urls")),  
 ]
