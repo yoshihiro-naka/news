6a4fe68d3d8619c7a13ce4a84541872dcf6be186
yoshihiro-naka <ynaka@rakuten.jp>
2024-03-31 01:44:03 +0900

    permissions and authorizations

--- articles/views.py
+++ articles/views.py
@@ -1,20 +1,21 @@
-from django.views.generic import ListView, DetailView  # new
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin  # new
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from django.urls import reverse_lazy  # new
+from django.urls import reverse_lazy
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):  # new
     model = Article
     template_name = "article_list.html"
 
 
-class ArticleDetailView(DetailView):  # new
+class ArticleDetailView(LoginRequiredMixin, DetailView):  # new
     model = Article
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):  # new
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):  # new
     model = Article
     fields = (
         "title",
@@ -22,18 +23,26 @@ class ArticleUpdateView(UpdateView):  # new
     )
     template_name = "article_edit.html"
 
+    def test_func(self):  # new
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):  # new
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):  # new
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
+    def test_func(self):  # new
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView):  # new
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):  # new
     model = Article
     template_name = "article_new.html"
-    fields = (
-        "title",
-        "body",
-        "author",
-	)
+    fields = ("title", "body")  # new
+
+    def form_valid(self, form):  # new
+        form.instance.author = self.request.user
+        return super().form_valid(form)
